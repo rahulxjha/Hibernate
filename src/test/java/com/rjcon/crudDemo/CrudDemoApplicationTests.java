faf0a2126d2d2aeb7a48f56b12010ee6c8da2192
Rahul Jha <rjrahuljha02@gmail.com>
2023-05-18 20:08:07 +0530

    commit

---
+++ src/test/java/com/rjcon/crudDemo/CrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rjcon.crudDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
